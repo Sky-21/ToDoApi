aba65b8c6022c6db847043c97ecb8f423f20f055
Allan <sky.dev212@gmail.com>
2020-04-23 19:35:10 -0300

    cors foi adicionado

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import Routes from './routes'
-
+import cors from 'cors'
 const port = process.env.PORT || 3333
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use(Routes)
